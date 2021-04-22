13a1e22499d9246a988d5f17259d413b869a0f9c
egiorganam <giorgana.elih@gmail.com>
2021-04-21 19:47:01 -0500

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const chalk = require('chalk')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/views/home.html`))
+
+app.get('/about', (req, res) => res.sendFile(`${__dirname}/views/about.html`))
+
+app.get('/works', (req, res) => res.sendFile(`${__dirname}/views/works.html`))
+
+app.listen(3000, () => console.log(chalk.red(chalk.bgCyanBright('Server Connected 🛸'))))
